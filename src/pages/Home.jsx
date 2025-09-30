9c6b08048330d7142dbfcaef379e684f69bb86a3
Kaz <kazuyasato01@yahoo.com>
2025-09-30 15:05:42 +0800

    initial commit
    initial commit for stellar-portfolio project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { HeroSection } from "../components/HeroSection";
+import { Navbar } from "../components/Navbar";
+import { StarBackground } from "../components/StarBackground";
+import { ThemeToggle } from "../components/ThemeToggle";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <ThemeToggle />
+      <StarBackground />
+      <Navbar />
+      <main>
+        <HeroSection />
+      </main>
+    </div>
+  );
+};
